ba53b5e5d52ddfc7172b2800df5468a2a645337a
Leo <Leo13480303758@163.com>
2021-07-22 10:33:41 +0800

    first commit

---
+++ src/main/java/com/leo/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.leo.git;
+
+/**
+ * @author Leo
+ * @create 2021-07-22 10:29
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
