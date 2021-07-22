145f4913a6767dc60e9a6c82b000b41ea2c5e1a6
Leo <Leo13480303758@163.com>
2021-07-22 11:04:33 +0800

    master commit2

--- src/main/java/com/leo/git/GitTest.java
+++ src/main/java/com/leo/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("Hello GitHub!");
         System.out.println("Hello GitHub3!");
         System.out.println("Hello GitHub4!");
+        System.out.println("master test!");
     }
 
 }
